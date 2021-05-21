76d60872c8404f150363091f3351d318181f26b1
Baira Santaeva <bsantaeva@microsoft.com>
2021-05-21 23:11:30 +0300

    Updates

---
+++ precache-manifest.8849f8092c4af32520343c6709ca3182.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aafe33b27fed819e2e6d81dc06215fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0af5d5582f7713447a5",
+    "url": "/static/css/main.c4b4f562.chunk.css"
+  },
+  {
+    "revision": "be65ea750fea28050391",
+    "url": "/static/js/2.7ff42729.chunk.js"
+  },
+  {
+    "revision": "a4d64d8080146665eb1e",
+    "url": "/static/js/3.05dd458b.chunk.js"
+  },
+  {
+    "revision": "a0af5d5582f7713447a5",
+    "url": "/static/js/main.ff92f6bf.chunk.js"
+  },
+  {
+    "revision": "40ddd44161cbb3faa58a",
+    "url": "/static/js/runtime-main.1ded8bc9.js"
+  },
+  {
+    "revision": "21ffb6bbef31e30abb8be384eb553611",
+    "url": "/static/media/main-header.21ffb6bb.png"
+  }
+]);
